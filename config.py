19d1ab564db848be3dfd857b0a87fefe1e1b5b3b
kamoliddinov25032010-del <kamoliddinov25032010@gmail.com>
2026-09-06 11:19:32 +0500

    Refactor ADMIN_IDS to ADMIN_ID
    Changed ADMIN_IDS list to a single ADMIN_ID.

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-ADMIN_IDS = [6857995319, 7695822564]
+ADMIN_ID = 6857995319
 
 # Railway'da Volume ulanganda shu papkaga yozadi (masalan "/data")
 # Volume bo'lmasa, joriy papkada saqlaydi (lokal ishlatish uchun)
