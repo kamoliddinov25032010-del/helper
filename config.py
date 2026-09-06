e51417fc96f4dbd6a56f593a20e2e1c278a27745
kamoliddinov25032010-del <kamoliddinov25032010@gmail.com>
2026-09-06 11:14:52 +0500

    Update config.py

--- config.py
+++ config.py
@@ -4,14 +4,14 @@ from dotenv import load_dotenv
 load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-ADMIN_ID = 6857995319
 
-# Railway'da Volume ulanganda shu papkaga yozadi (masalan "/data")
-# Volume bo'lmasa, joriy papkada saqlaydi (lokal ishlatish uchun)
+ADMIN_IDS = [
+    6857995319,
+    7695822564  # 2-admin ID
+]
+
 DATA_DIR = os.getenv("DATA_DIR", ".")
 
-# Papka mavjudligiga ishonch hosil qilamiz (Volume ulanganda ham,
-# ulanmaganda ham) - aks holda fayl yozishda xato chiqishi mumkin
 try:
     os.makedirs(DATA_DIR, exist_ok=True)
     print(f"📁 DATA_DIR: {os.path.abspath(DATA_DIR)}")
